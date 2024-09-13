ab3ed99ecb2adb5a83fa20f45bc114c4efec4bb7
Tisya V <tisya.varghese22@imperial.ac.uk>
2024-09-13 11:46:53 +0100

    Updates

--- rubberduck/components/constants.jsx
+++
@@ -1,5 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export const screenHeight = Dimensions.get('window').height;
-export const screenWidth = Dimensions.get('window').width;
-export const flashcardDir = 'resources/decks/';
